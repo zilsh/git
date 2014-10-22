ohmyzsh/ohmyzsh

Fix gitignore curl, should follow redirects

plugins/gitignore/gitignore.plugin.zsh
@@ -1,4 +1,4 @@
-function gi() { curl http://www.gitignore.io/api/$@ ;}
+function gi() { curl -L http://www.gitignore.io/api/$@ ;}
 
 _gitignireio_get_command_list() {
   curl -s http://www.gitignore.io/api/list | tr "," "\n"

zilsh/git

plugins/gitignore/gitignore.plugin.zsh
@@ -1,4 +1,4 @@
-function gi() { curl https://www.gitignore.io/api/$@ ;}
+function gi() { curl -L https://www.gitignore.io/api/$@ ;}
 
 _gitignoreio_get_command_list() {
   curl -s https://www.gitignore.io/api/list | tr "," "\n"